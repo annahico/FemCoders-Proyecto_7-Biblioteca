22fbd207c1238305bf5716a1b683a7edd0237e84
GeraldineSaco <gsacoruiz@gmail.com>
2026-02-06 12:04:26 +0100

    feat: add import to BookServive.java file

--- src/main/java/com/library/service/BookService.java
+++ src/main/java/com/library/service/BookService.java
@@ -1,6 +1,9 @@
 package com.library.service;
 
-public class BookService {
+import java.util.List;
+import com.library.model.Book;
+
+public interface BookService {
     void createBook(Book book);
     void updateBook(Book book);
     void deleteBook(String isbn);
